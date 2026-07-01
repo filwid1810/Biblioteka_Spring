024e61957cc63774a42730d9e27054a26870e478
N1S4H <joanna.radom12@gmail.com>
2026-07-01 14:51:56 +0200

    2

--- src/main/java/com/umcsuser/carrent/BookShopApplication.java
+++ src/main/java/com/umcsuser/carrent/BookShopApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarRentApplication {
+public class BookShopApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CarRentApplication.class, args);
+        SpringApplication.run(BookShopApplication.class, args);
     }
 }
